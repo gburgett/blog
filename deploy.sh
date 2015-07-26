3f3bee19922aafa6cb23c401707fcd5888a33c3e
Burgett, Gordon <Gordon.Burgett@gmail.com>
2015-07-25 20:11:29 -0500

    changed to use haproxy rather than load balancer

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+user=ec2-user
+server=ec2-54-149-172-147.us-west-2.compute.amazonaws.com
+
 hugo --baseUrl="http://www.gordonburgett.net"
-rsync -az -e ssh --progress public/ ec2-user@ec2-54-149-172-147.us-west-2.compute.amazonaws.com:~/blog
-rsync -e ssh package.json ec2-user@ec2-54-149-172-147.us-west-2.compute.amazonaws.com:~/
+rsync -az -e ssh --progress public/ $user@$server:~/blog
+rsync -e ssh package.json $user@$server:~/
+rsync -e ssh haproxy.conf $user@$server:~/
+
+ssh -t $user@$server 'bash -c "sudo haproxy -f haproxy.conf -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)"'
