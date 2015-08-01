bbae3fbb8515bdc7b0b7eb2420739c071eebfbc4
Burgett, Gordon <Gordon.Burgett@gmail.com>
2015-07-31 21:37:11 -0500

    initial draft of journal

--- static/js/custom.js
+++ static/js/custom.js
@@ -9,4 +9,19 @@ $(document).ready(function(){
 		$('#email').append(link);
 		$('#email').off();
 	})
+
+	$( "img.data-img" ).each(function() {
+	  var attr = $(this).attr('data-image-src');
+
+	  if (typeof attr !== typeof undefined && attr !== false) {
+	    if (window.matchMedia && window.matchMedia("(max-width: 640px)").matches){
+			arr = attr.split('.')
+			end = arr.pop()
+			arr.push('640x', end)
+	      $(this).attr('src', arr.join('.'));
+	    } else {
+	      $(this).attr('src', attr);
+	    }
+	  }
+	});
 });
