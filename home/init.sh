1bba57dc3aa549b98a79b9e02a1aaa25161c79e3
Gordon Burgett <gordon.burgett@gmail.com>
2016-02-14 18:59:37 +0100

    updated haproxy.cfg to use dns names for routing services

--- home/init.sh
+++ home/init.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-# start haproxy
-sudo yum -y install haproxy
-sudo /usr/sbin/haproxy -f /home/ec2-user/hosting/haproxy.conf -D
-
 # set up ecs
 sudo mv ~/hosting/ecs.config /etc/ecs/ecs.config
 
@@ -15,6 +11,3 @@ curl -Ls https://get.cloud.docker.com/ | sudo -H sh -s 595eca1a930246a7a6afb990a
 # install squid proxy & configure
 sudo yum -y install squid
 sudo cp ~/hosting/squid.conf /etc/squid/squid.conf
-
-# do an update which is running in the background while we've at least got things running
-sudo yum update
