bf7357b0ec55298a13f7166ec96605cbac990e7c
Gordon Burgett <gordon.burgett@gmail.com>
2016-02-14 18:42:14 +0100

    adding docker-cloud and creating an HAProxy container

--- home/hosting/update_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[[ -z "$1" ]] && echo "container name expected" && exit 1
-
-IMAGE="$1"
-echo "restarting $IMAGE"
-docker ps | grep $IMAGE | awk '{print $1}' | xargs docker stop
-docker pull $IMAGE
-docker run --restart=always -d -p 8080:8080 -p 8081:8081 $IMAGE
--- home/init.sh
+++ home/init.sh
@@ -6,18 +6,15 @@ set -ex
 sudo yum -y install haproxy
 sudo /usr/sbin/haproxy -f /home/ec2-user/hosting/haproxy.conf -D
 
-# start the blog and the reboot listener
-docker run --restart=always -d -p 8080:8080 -p 8081:8081 gordonburgett/blog
-
-uuid="30b96b9c-bff4-4f6b-a0fc-df88d045c8a2"
-curl https://raw.githubusercontent.com/schickling/docker-hook/master/docker-hook > /home/ec2-user/hosting/docker-hook; chmod +x /home/ec2-user/hosting/docker-hook
-nohup /home/ec2-user/hosting/docker-hook -t $uuid -c /bin/bash /home/ec2-user/hosting/update_container.sh &
-
-
 # set up ecs
 sudo mv ~/hosting/ecs.config /etc/ecs/ecs.config
 
+# set up docker cloud
+curl -Ls https://get.cloud.docker.com/ | sudo -H sh -s 595eca1a930246a7a6afb990abb019ba
 
 # install squid proxy & configure
 sudo yum -y install squid
 sudo cp ~/hosting/squid.conf /etc/squid/squid.conf
+
+# do an update which is running in the background while we've at least got things running
+sudo yum update
