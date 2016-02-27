f4148a29caba38d9862c4c1314ed665666040c50
Gordon Burgett <gordon.burgett@gmail.com>
2016-02-27 15:49:34 +0100

    New post sometimes_its_tough.md

--- home/init.sh
+++ home/init.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+cd /home/ubuntu
 sudo apt-get update
 
 # install squid proxy & configure
@@ -18,8 +19,17 @@ docker-cloud node byo | grep 'curl' | sh
 sudo gpasswd -a ubuntu docker
 
 # set up stats logging
-$ sudo apt-get -y install libwww-perl libdatetime-perl
+sudo apt-get -y install libwww-perl libdatetime-perl
 curl http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip -o ~/CloudWatchMonitoringScripts-1.2.1.zip
 unzip ~/CloudWatchMonitoringScripts-1.2.1.zip -d ~/
 rm ~/CloudWatchMonitoringScripts-1.2.1.zip
-echo '*/5 * * * * ubuntu /home/ubunt u/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --swap-util --sw ap-used --disk-space-util --disk-space-used --disk-path=/ --auto-scaling=only --from-cron' | sudo  tee /etc/cron.d/metrics > /dev/null
+echo '*/5 * * * * ubuntu /home/ubuntu/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --swap-util --swap-used --disk-space-util --disk-space-used --disk-path=/ --auto-scaling=only --from-cron' | sudo tee /etc/cron.d/metrics > /dev/null
+
+# download letsencrypt
+sudo apt-get -y install git
+git clone https://github.com/letsencrypt/letsencrypt
+letsencrypt/letsencrypt-auto --help
+sudo install letsencrypt/letsencrypt-auto /usr/bin/letsencrypt
+sudo mkdir -p /var/letsencrypt/www
+sudo aws s3 cp --recursive s3://gordonburgett.net/letsencrypt /etc/letsencrypt/
+sudo chown -R root:root /etc/letsencrypt
--- run_draft.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-hugo server --theme=hugo-uno --buildDrafts --watch
