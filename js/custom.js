e271c5ec491757115c405d6119969b59fbdd7692
gburgett <gburgett@users.noreply.github.com>
2024-10-06 18:30:14 +0000

    deploy: e78203ec357c849c254dc6cb59512a308279245b

--- js/custom.js
+++ js/custom.js
@@ -9,17 +9,4 @@ $(document).ready(function(){
 		$('#email').append(link);
 		$('#email').off();
 	})
-
-	$( "img" ).each(function() {
-	  var attr = $(this).attr('src');
-
-	  if (typeof attr !== typeof undefined && attr !== false) {
-	    if (!window.matchMedia || !window.matchMedia("(max-width: 640px)").matches){
-	      if (attr.indexOf('/.640x')  > -1){
-	      	attr = attr.replace('/.640x', '')
-	      	$(this).attr('src', attr);
-	      }
-	    }
-	  }
-	});
 });
